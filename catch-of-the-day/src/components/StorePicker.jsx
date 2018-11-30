eebf637d7ffc3fc9536dbf50210932dd076fe1ae
Luís Lopes <luis.lopes@armis.pt>
2018-11-30 22:52:27 +0000

    Lesson 4

--- catch-of-the-day/src/components/StorePicker.jsx
+++ catch-of-the-day/src/components/StorePicker.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
-export default class StorePicker extends React.Component{
-    render(){
-        return <p>Hello!</p>
+export default class StorePicker extends React.Component {
+    render() {
+        return (
+        <form action="" className="store-selector">
+            <h2>Please Enter a Store</h2>
+            <input type="text" className="text" required placeholder="Store Name"/>
+            <button type="submit">Visit Store -></button>
+        </form>
+        )
     }
 }
