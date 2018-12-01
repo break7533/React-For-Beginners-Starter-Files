f57950b2ecd35f2e557a1f5fe8938ef72ac65a9c
Luís Lopes <luis.lopes@armis.pt>
2018-12-01 00:19:59 +0000

    Lesson 10 - Helper Functions

--- catch-of-the-day/src/components/StorePicker.jsx
+++ catch-of-the-day/src/components/StorePicker.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
+import { getFunName } from "../helpers";
 
 export default class StorePicker extends React.Component {
-    render() {
-        return (
-        <form action="" className="store-selector">
-            <h2>Please Enter a Store</h2>
-            <input type="text" className="text" required placeholder="Store Name"/>
-            <button type="submit">Visit Store →</button>
-        </form>
-        )
-    }
+  render() {
+    return (
+      <form action="" className="store-selector">
+        <h2>Please Enter a Store</h2>
+        <input type="text" className="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+        <button type="submit">Visit Store →</button>
+      </form>
+    );
+  }
 }
