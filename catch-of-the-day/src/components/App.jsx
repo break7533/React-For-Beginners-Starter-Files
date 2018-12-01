fd5f2ccea203c8e309be85d96c642da6ad91cbe1
Luís Lopes <luis.lopes@armis.pt>
2018-12-01 01:36:49 +0000

    Lesson 13 - State and Managing Page State

--- catch-of-the-day/src/components/App.jsx
+++ catch-of-the-day/src/components/App.jsx
@@ -4,14 +4,28 @@ import Inventory from "./Inventory";
 import Order from "./Order";
 
 export default class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    const prevFishes = { ...this.state.fishes };
+
+    prevFishes[`fish${Date.now()}`] = fish;
+    this.setState({
+      fishes: prevFishes
+    });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
         </div>
-        <Inventory />
         <Order />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
